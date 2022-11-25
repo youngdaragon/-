cb62b0c9e4d95fbeb03838dc51bd1af6c31934a0
youngdaragon <93703248+youngdaragon@users.noreply.github.com>
2022-11-25 16:26:41 +0900

    기능 추가 구현(동적할당) 출력 변경

--- data_struct_list.c
+++ data_struct_list.c
@@ -4,146 +4,175 @@
 typedef struct _student
 {
     int student_id;
-    char *name;
-    int class_register[5];
+    const char *name;
+    int class_register[100];
+    int score[100];
 } Student;
-
+//학생 구조체 선언
 typedef struct _snode
 {
     struct _student* link;
     struct _snode* next;
     struct _snode* prev;
-} Snode;
+} Snode; 
+//node를 선언해주는 구조체 link 는 학생구조체 나머지는 이전과 다음 node의 주소를 가르킴
 
-typedef struct _classes
+typedef struct _class
 {
     int class_id;
-    char *class_name;
+    const char* class_name;
     int num_registered;
     int avg_score;
-    Snode* student_head, *student_tail;
-} Classes;
+    Snode* student_head, * student_tail;
+} Class;
+//class의 구조체 내부에는 클래스 id, 이름, 몇명이 등록했는가 평균점수, 
+// head, tail을 입력해서 student_linked list에서 정보를 가져옴
 
 void init_Snode(Snode **head, Snode **tail, int num_class);
-void init_Student(Student* studentarray);
-void init_Class(Classes *classes, Snode** student_head, Snode** student_tail);
-void apply_class(Classes *classes, Student* studentarray, int num_student,int num_class);
-Snode* add_student_to_class(Classes *classes, Student* studentarray, int s_id);
-void print_info(Classes *classes, int num_class);
-
+void init_Student(Student* studentarray, int num_student);
+void init_Class(Class *c, Snode** student_head, Snode** student_tail, int num_class);
+void apply_class(Class *c, Student* studentarray,int num_student, int num_class);
+Snode* add_student_to_class(Class* c, Student* studentarray, int s_id);
+void print_info(Class* c, int num_class);
+//함수 선언
 int main()
 {
-    int num_student=2;
-    int num_class=2;
+    // int num_student=2;
+    // int num_class=2;
+    int num_student;
+    printf("number of student: ");
+    scanf("%d",&num_student);
+    int num_class;
+    printf("number of class: ");
+    scanf("%d",&num_class);
+    //학생 수와 class 수를 동적으로 받아준다.
     Student *studentarray;
-    Snode** student_head, **student_tail;
-    Classes* classes;
-
-    student_head = (Snode**)malloc(num_class*sizeof(Snode *));
+    Snode** student_head, ** student_tail;
+    Class* c;
+    //구조체에 의해서 각각의 구조체를 선언해준다.
+    student_head=(Snode**)malloc(num_class*sizeof(Snode *));
     for (int i=0; i<num_class; i++)
+    {
         student_head[i]=(Snode*)malloc(sizeof(Snode));
-
-    student_tail=(Snode**)malloc(num_class*sizeof(Snode*));
+    }
+    //student_head 부분에서  malloc을 이용해 num_class만큼 동적할당을 시켜주고 head의 각 크기는
+    //다시 각 num_class만큼 동적할당 시킨 head를 student_head를 동적할당 시켜준다.
+    student_tail=(Snode**)malloc(num_class*sizeof(Snode *));
     for (int i=0; i<num_class; i++)
+    {
         student_tail[i]=(Snode*)malloc(sizeof(Snode));
-    
-    classes=(Classes*)malloc(num_class*sizeof(Classes));
-    studentarray=(Student*)malloc(num_student*sizeof(Student));
+    }
+    //tail도 head와 마찬가지로 동적할당 시켜준다.
 
-    init_Snode(student_head, student_tail, num_class);
-    init_Class(classes, student_head, student_tail);
-    init_Student(studentarray);
-    apply_class(classes,studentarray,num_student,num_class);
-    print_info(classes,num_class);
+    c=(Class*)malloc(num_class*sizeof(Class));
+    studentarray=(Student*)malloc(num_student*sizeof(Student));
+    //class 구조체를 담을 c를 동적할당을 이용해 만들어준다.
+    //studentarray를 동적할당을 이용해 num_student만큼 만들어준다. 
+
+    init_Snode(student_head,student_tail,num_class);
+    init_Class(c, student_head, student_tail, num_class);
+    init_Student(studentarray, num_student);
+    apply_class(c,studentarray,num_student,num_class);
+    print_info(c,num_class);
+    //각각 함수에 위에서 정의해준 값을 넣어준다.
 
     return 0;
 }
 
-void init_Snode(Snode** head, Snode** tail, int num_class)
+void init_Snode(Snode** head,Snode** tail,int num_class)
 {
     for(int i=0; i<num_class; i++)
     {
-        head[i]->next=tail[i];
+        head[i] ->next =tail[i];
         head[i]->prev=head[i];
         tail[i]->next=tail[i];
         tail[i]->prev=head[i];
     }
-}
+}//Snode의 초기값을 정해 줄 때 각각 과목 수마다 head와 tail을 정의해준다.
+//head 다음은 tail, 전은 head, tail 이전은 head, 다음은 tail.
 
-void init_Student(Student* studentarray)
+void init_Student(Student *studentarray,int num_student)
 {
-    studentarray[0].student_id=1;
-    studentarray[0].name="kim";
-    studentarray[1].student_id=2;
-    studentarray[1].name="lee";
-}
+    for (int i=0; i<num_student; i++)
+    {
+        studentarray[i].student_id=i+1;
+        printf("name of student: ");
+        scanf("%s",&studentarray[i].name);
+    }
+}//student의 구조체의 num_student만큼 studentarray를 만들어준다.
 
-void init_Class(Classes *classes, Snode** student_head, Snode **student_tail)
+void init_Class(Class *c, Snode** student_head, Snode **student_tail, int num_class)
 {
-    classes[0].class_id=0;
-    classes[0].class_name="math";
-    classes[0].num_registered=0;
-    classes[0].avg_score=0;
-    classes[0].student_head=student_head[0];
-    classes[0].student_tail=student_tail[0];
+    for (int i=0; i<num_class; i++)
+    {c[i].class_id=i+1;
+    printf("class of name: ");
+    scanf("%s",&c[i].class_name);
+    c[i].num_registered=0;
+    c[i].avg_score=0;
+    c[i].student_head=student_head[i];
+    c[i].student_tail=student_tail[i];}
+}//과목수 마다 class구조체를 만들어 준다. 각각의 값을 넣어준다.
 
-    classes[1].class_id=1;
-    classes[1].class_name="eng";
-    classes[1].num_registered=0;
-    classes[1].avg_score=0;
-    classes[1].student_head=student_head[1];
-    classes[1].student_tail=student_tail[1];
-}
-
-void apply_class(Classes* classes ,Student *studentarray, int num_student,int num_class){
+void apply_class(Class* c, Student *studentarray, int num_student, int num_class)
+{
     int apply;
     for(int s=0; s<num_student; s++)
     {
-        printf(" \n Apply classes for student with id %d ",s);
-        printf(" Input 1 if applying 0 otherwise \n");
-        for (int c=0; c<num_class; c++)
+        printf("\n Apply classes student name : %s.",&studentarray[s].name);
+        printf("input 1 if applying 0 otherwise \n");
+        for (int j=0; j<num_class; j++)
         {
-            printf("\n class %s : ",classes[c].class_name);
-            scanf("%d",&apply);
-            if(apply==1)
+            printf("\n class %s : ",&c[j].class_name);
+            scanf("%d",&apply); //수강을 하면 1, 아니면 0
+            if (apply==1)
             {
-                studentarray[s].class_register[c]=1;
-                add_student_to_class(&classes[c],studentarray,s);
-            }
+                printf("\n score: ");
+                scanf("%d",&studentarray[s].score[j]);//점수를 받는 코드
+                studentarray[s].class_register[j]=1;
+                add_student_to_class(&c[j],studentarray,s);
+            }//수강을 할 경우에는 레지스터 값에 1이 들어가고 add_student_to_class에 class 구조체 값과
+            //studentarray와 몇 번째 student인지를 넣어준다.
             else
-                studentarray[s].class_register[c]=0;           
+            {
+                studentarray[s].class_register[j]=0;
+            }//수강을 안할경우에는 studentarray에만 0값이 저장 되고 끝난다.
         }
     }
 }
 
-Snode *add_student_to_class(Classes* classes, Student *studentarray, int s_id)
+Snode* add_student_to_class(Class *c, Student* studentarray, int s_id)
 {
-    Snode* node;
-    classes->num_registered++;
-    classes->student_head;
-    node=(Snode*)malloc(sizeof(Snode));
-    node->link =&studentarray[s_id];
-    classes->student_head->next->prev=node;
-    node->next=classes->student_head->next;
-    node->prev=classes->student_head;
-    classes->student_head->next=node;
+    Snode* node; //구조체를 선언해준다.
+    c->num_registered++; //c에 num_registered를 1 추가시켜준다.
+    node=(Snode*)malloc(sizeof(Snode)); //node를 동적할당을 시켜준다.
+    node->link=&studentarray[s_id];// node의 link 부분은 studentarray[s_id]수강을 듣는 학생의 배열 index를 가르킨다.
+    c->student_head->next->prev=node;// head의 다음부분의 그 전을 node로 지정한다.
+    node->next=c->student_head->next;//노드의 다음 부부분은 head의 다음부분으로 지정한다.
+    node->prev=c->student_head;//노드의 전은 헤드로 지정한다.
+    c->student_head->next=node;//헤드의 다음은 노드로 지정한다.
 }
 
-void print_info(Classes *classes, int num_class)
+void print_info(Class* c, int num_class)
 {
     Snode* ptr;
 
-    for (int c=0; c<num_class; c++)
+    for(int j=0; j<num_class; j++)
     {
-        ptr=classes[c].student_head->next;
-        printf(" registered students for class id %d are :\n",c);
+        // ptr=c[j].student_head->next;
+        ptr=c[j].student_tail->prev; //tail에서 시작해서 prev로 거꾸로 해준다.
+        printf("registered students for class name %s are: \n",&c[j].class_name);
         int k=0;
-        while(ptr != classes[c].student_tail)
+        while(ptr != c[j].student_head) //student_head가 ptr일때 멈춘다.
         {
             k++;
-            printf(" ( %d-th id=%d name= %s ) \n", k, ptr->link->student_id, ptr->link->name);
-            ptr=ptr->next;
-        }
-
+            printf("(%d-th id=%d name=%s score=%d) \n", 
+                    k,ptr->link->student_id,&(ptr->link->name),ptr->link->score[j]); 
+            c[j].avg_score+=ptr->link->score[j];
+            //내부정보를 linked list를 돌면서 출력
+            ptr=ptr->prev; //ptr이 이전 prev를 가르키도록 한다.
+            //이럴 경우 학번순으로 나오는 것이 가능하다. (그 전에는 학번 역순으로 출력)
+        } 
+        c[j].avg_score=c[j].avg_score/c[j].num_registered;//평균 score를 계산한다.
+        printf("%s is avg_score: %d \n",&c[j].class_name,c[j].avg_score);//평균 score를 출력한다.
     }
 }
--- data_struct_stack.c
+++ data_struct_stack.c
@@ -1,176 +1,208 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 typedef struct _maze
 {
     int x,y;
     char value;
     int visited; // 1 visited, 0 unvisited
-} Maze;
+} Maze; //미로 기본틀 설정
 
 typedef struct _stack
 {
     int top;
-    int fork_x[10], fork_y[10];
-} StackArray;
+    int fork_x[100],fork_y[100];
+} StackArray; // 미로를 빠져나가기 위한 스택 기본설정
+
+typedef struct _stack1
+{
+    int top1;
+    int path_x[100],path_y[100];
+} StackPath; // 경로를 찾기위한 스택 기본설정
 
 int *init_Maze(Maze maze[][10]);
-int find_path(Maze maze[][10], StackArray *fork_stack, int *start_coordi);
-int* find_next_point(Maze(*maze)[10], int* prev_coordi, StackArray *fork_stack);
+int find_path(Maze maze[][10],StackArray *fork_stack,int *start_coordi,StackPath *path_stack);
+int* find_next_point(Maze(*maze)[10],int* prev_coordi, StackArray *fork_stack,StackPath *path_stack);
+void print_stack(StackPath *path_stack);
 
 int main()
 {
-    Maze maze[10][10];
+    Maze maze[10][10]; //미로 크기 설정
     StackArray fork_stack;
-    int *start_coordi;
-    fork_stack.top=-1;
-    start_coordi=init_Maze(maze);
-    find_path(maze,&fork_stack,start_coordi);
+    StackPath path_stack;
+    int* start_coordi;
+    fork_stack.top =-1; //fork_stack top -1 설정
+    path_stack.top1=-1; //path_stack top -1 설정
+
+    start_coordi=init_Maze(maze); // 미로만드는 함수에 대입
+    find_path(maze,&fork_stack,start_coordi,&path_stack); //경로찾는 함수에 대입
+}
+
+void print_stack(StackPath *path_stack)
+{
+    int i;
+    int top1=path_stack->top1;
+    for (i=0;i<=top1;i++)
+    {
+        printf("(%d, %d) \n",path_stack->path_x[i],path_stack->path_y[i]);
+    }
+    //stack에 있는 것을 print 해주는 함수
 }
 
 int *init_Maze(Maze maze[][10])
 {
     int i,j;
-    int *start_coordi;
-    start_coordi=(int*)malloc(2*sizeof(int));
+    int* start_coordi;
+    start_coordi=(int*)malloc(2*sizeof(int)); //메모리 동적 할당
 
-    for(i=0; i<10; i++)
-    {
-        for(j=0; j<10; j++)
+    for(i=0;i<10;i++)
+        for(j=0;j<10;j++)
         {
             maze[i][j].value='1';
-            maze[i][j].visited='0';
+            maze[i][j].visited='0'; //미로의 첫 visited는 0으로 설정
         }
-    }
-    for(j=0; j<5; j++)
-        maze[1][j].value='0';
-    for(j=5; j<10; j++)
-        maze[3][j].value='0';
-    for(i=2; i<8;i++)
-    {
-        maze[i][2].value='0';
-        maze[i][7].value='0';
-    }
+    for(j=0;j<5;j++)
+            maze[1][j].value='0';
+    for(j=5;j<10;j++)
+            maze[3][j].value='0';
+    for(i=2;i<8;i++)
+        {
+            maze[i][2].value='0';
+            maze[i][7].value='0';
+        }
     maze[1][5].value='0';
     maze[2][5].value='0';
 
-    for(i=0; i<10; i++)
-    {
-        for(j=0;j<10;j++)
-            printf(" %c", maze[i][j].value);
-        printf("\n");
-    }
+    for(i=0;i<10;i++)
+        {
+            for(j=0;j<10;j++)
+            {
+                printf(" %c ",maze[i][j].value);
+            }
+            printf(" \n");
+        }
     start_coordi[0]=1;
     start_coordi[1]=0;
+    maze[start_coordi[0]][start_coordi[1]].visited=1; //시작점의 visited가 1이 되도록 설정
     return start_coordi;
 }
 
-int find_path(Maze maze[][10], StackArray *fork_stack, int *start_coordi)
+int find_path(Maze maze[][10], StackArray *fork_stack, int *start_coordi,StackPath *path_stack)
 {
-    int *next_coordi; 
-    int *prev_coordi;
+    int *next_coordi; //다음 좌표값
+    int *prev_coordi; //이전 좌표값
     next_coordi=(int*)malloc(2*sizeof(int));
     prev_coordi=(int*)malloc(2*sizeof(int));
-    prev_coordi[0]=start_coordi[0];
-    prev_coordi[1]=start_coordi[1];
+    prev_coordi[0]=start_coordi[0]; //초기x값 대입
+    prev_coordi[1]=start_coordi[1]; //초기y값 대입
 
     while(1)
     {
-        printf("visit : ( %d %d )\n",prev_coordi[0],prev_coordi[1]);
-        next_coordi=find_next_point(maze,prev_coordi,fork_stack);
+        printf("visit : ( %d %d)\n", prev_coordi[0],prev_coordi[1]); //방문한 포인트 찍기
+        next_coordi=find_next_point(maze,prev_coordi,fork_stack,path_stack); //다음에 가는 좌표 구하기
+        int top1=path_stack->top1; //경로찾는 stack top 추출
         //push to stack if fork & select a next point
-        if (next_coordi[0]>=9 || next_coordi[1]>=9)
+        if (next_coordi[0]>=9 || next_coordi[1]>=9) //마지막 행,열에 도달하면 탈출
         {
-            printf(" Exit point (%d, %d) reached, \n",next_coordi[0],next_coordi[1]);
-            //print_path();
+            printf("Exit point (%d %d) reached, \n",next_coordi[0],next_coordi[1]);//마지막 좌표값 추출
+            printf("\n path point is \n");
+            printf("(%d, %d)\n",start_coordi[0],start_coordi[1]);
+            print_stack(path_stack);
             return 1;
         }
-        else //proceed to the next point
+        else // proceed to the next point
         {
-            prev_coordi=next_coordi;
+            prev_coordi=next_coordi; //prev를 next로 바꾸는 코드
         }
     }
     return 1;
 }
 
-int * find_next_point(Maze (*maze)[10], int *prev_coordi, StackArray *fork_stack)
+int * find_next_point(Maze(*maze)[10],int *prev_coordi, StackArray *fork_stack,StackPath *path_stack)
 {
-    int x= prev_coordi[0];
-    int y= prev_coordi[1];
-    int next_x,next_y;
-    int *coordi;
+    int x=prev_coordi[0];//입력된 x
+    int y=prev_coordi[1];//입력된 y
+    int next_x,next_y;//다음 좌표값
+    int *coordi;//좌표값
     coordi=(int*)malloc(2*sizeof(int));
-    int fork_flag=0;
+    int fork_flag=0;//갈림길 표시 숫자
 
-    for (int i=0; i<4; i++)
+    for(int i=0;i<=4;i++)
     {
         if (i==0)
         {
             next_x=x-1;
             next_y=y;
-        }
-        else if (i==1)
+        } // 한칸 위로 움직임
+        else if(i==1)
         {
             next_x=x;
             next_y=y+1;
-        }
-        else if (i==2)
+        } //한칸 오른쪽으로 움직임
+        else if(i==2)
         {
             next_x=x+1;
             next_y=y;
-        }
-        else if (i==3)
+        }//한칸 아래로 움직임
+        else if(i==3)
         {
             next_x=x;
             next_y=y-1;
-        }
+        }//한칸 왼쪽으로 움직임
         if (next_x<0 || next_y<0)
         {
             continue; //boundary
-        }
-        if (next_x<0 || next_y<0)
+        }//0보다 작은 좌표가 나오면 for loop로 돌아감
+        else if(next_x>=10 || next_y>=10)
         {
-            continue; //dead end
-        }
+            continue; //boundary
+        }//10보다 크면 반복문으로 돌아감
         else if(maze[next_x][next_y].value=='1')
         {
             continue; //dead end
-        }
+        }//막힌벽이면 좌표면 돌아감
         else if(maze[next_x][next_y].visited==1)
         {
-            continue; //value = 0
-        }
+            continue; //dead end
+        }//방문했던 좌표면 돌아감
         else if(fork_flag==0)
         {
             maze[next_x][next_y].visited=1;
             coordi[0]=next_x;
             coordi[1]=next_y;
             fork_flag=1;
-        }
-        else if(fork_flag==1)//value=0, visited=0
+        }//만약 전진했을때 위 해당사항이 없으면 fork_flag를 1로 설정하고 좌표는 다음값으로 설정
+        else if(fork_flag==1) //value=0,visited=0
         {
             fork_stack->top++;
             fork_stack->fork_x[fork_stack->top]=x;
             fork_stack->fork_y[fork_stack->top]=y;
-            printf(" stack push( %d %d ) top = %d \n", x, y, fork_stack->top);
+            printf("fork stack push (%d %d) top= %d \n", x, y, fork_stack->top);
             break;
-        }
+        }//만약 앞서간 경로 이외에도 0이 있다면(갈림길이라면) fork_stack에 좌표를 쌓아둠 
     }
-    if(fork_flag==1) //found next point
+    if (fork_flag==1) //found next point
     {
+        path_stack->top1++;
+        path_stack->path_x[path_stack->top1]=coordi[0];
+        path_stack->path_y[path_stack->top1]=coordi[1];
+        printf("path stack push (%d %d) top= %d \n", coordi[0], coordi[1], path_stack->top1);
         return coordi;
+        //지나갔던 경로들을 stack_path에 쌓아둠
     }
     else //dead end
-    {
-        int top=fork_stack->top;
-        if (top>=0)
+    { //만약 벽을 만났을 때 pop을 이용한 돌아가는 코드
+        int top =fork_stack->top;
+        int top1=path_stack->top1; // 각 stack들에 top을 추출
+        if(top>=0) //top이 0보다 클때만 적용
         {
             coordi[0]=fork_stack->fork_x[top];
             coordi[1]=fork_stack->fork_y[top];
-            fork_stack->top--;
-            printf(" pop stack (%d %d )\n",coordi[0],coordi[1]);
+            fork_stack->top--; //좌표값에 top의 좌표값을 넣어주고 stack에서 top의 수를 낯춰 없애준다. 
+            printf("pop fork_stack (%d %d) \n", coordi[0], coordi[1]);
+            path_stack->top1--; //좌표값에 top의 좌표값을 넣어주고 stack에서 top의 수를 낯춰 없애준다. 
+            printf("pop path_stack (%d %d) \n", coordi[0], coordi[1]);
             return coordi;
-
         }
     }
 }
